a3a0a3a31a876e8043528bcf35ad8b6c19d935a4
mauromltn <mauromtne@icloud.com>
2025-05-10 00:43:11 +0200

    fix: Refactor Hero component button to use anchor tag for better semantics

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,14 +7,16 @@ export default function Hero() {
          I build-responsive, high-performance web apps—using clean code, UX best practices, and tailored UI design.
       </span>
       <div className='flex gap-10 items-center'>
-         <button className='bg-white ring ring-neutral-300 font-semibold py-4 px-5 rounded-full hover:shadow-[0px_8px_12px_0px_rgba(0,0,0,0.105)] hover:scale-101 duration-200 cursor-pointer'>
-            <a href="mailto:dev.montane@gmail.com">Get in touch</a>
-         </button>
+            <a
+               href="mailto:dev.montane@gmail.com"
+               className='bg-white ring ring-neutral-300 font-semibold py-4 px-5 rounded-full hover:shadow-[0px_8px_12px_0px_rgba(0,0,0,0.105)] hover:scale-101 duration-200'
+            >
+               Get in touch
+            </a>
+
          <div className='flex gap-3 items-center'>
-         <div className='bg-[#12C971] rounded-full w-3 h-3'></div>
-         <p className='text-[#088046] font-medium'>
-            Open to New Opportunities
-         </p>
+            <div className='bg-[#12C971] rounded-full w-3 h-3'></div>
+            <p className='text-[#088046] font-medium'>Open to New Opportunities</p>
          </div>
       </div>
     </main>
